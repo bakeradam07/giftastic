72f2c764a6e82d17585a52fb84ff46592286cf9b
Adam Baker <albcodingbootcamp@gmail.com>
2019-10-15 14:47:31 -0400

    got the app to display 10 gifs and the rating.  need to get a function working for when clicking on the Gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
+
 var topic = ["Family Guy", "Simpsons", "South Park", "AFV", "Superstore", "Breaking Bad", "Ballers", "Entourage"];
 
-function renderButtons() {
+function tvshowButtons() {
 
 $("#tvshow-buttons").empty();
 
@@ -21,8 +22,51 @@ for (var i = 0; i < topic.length; i++) {
       
       topic.push(tvshow);
 
-      renderButtons();
+      tvshowButtons();
 
     });
 
-    renderButtons();
+    
+
+  tvshowButtons();
+
+  
+
+  function displayShowGif () {
+    var tvshow = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + tvshow + "&api_key=SoP1reCn3w8jX9cpGCQLyD0G9zKcIXaY&limit=10";
+
+    console.log(queryURL);
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      
+      console.log(response);
+      $("#tvshow-view").empty();
+
+      var gifs = response.data;
+
+      for (var i = 0; i < gifs.length; i++) {
+        var tvshowDiv = $("<div class='tvshow'>");
+        var rating = gifs[i].rating;
+        var displayRating = $("<p>").html("Rating: " + rating);
+        var showGIF = $('<img>');
+          showGIF.attr('src', gifs[i].images.fixed_height_still.url)
+
+
+        tvshowDiv.append(displayRating).append(showGIF);
+
+        $('#tvshow-view').prepend(tvshowDiv);
+      }
+
+      
+  });
+}
+
+
+$(document).on("click", ".tvshow", displayShowGif);
+
+
+// $("#tvshow-view").empty();

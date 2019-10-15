d577d2920fad6e2cf7d1ae34479b5378c20ac099
Adam Baker <albcodingbootcamp@gmail.com>
2019-10-15 11:58:37 -0400

    got the click function working to add a TV Show to the aray

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,28 @@
 var topic = ["Family Guy", "Simpsons", "South Park", "AFV", "Superstore", "Breaking Bad", "Ballers", "Entourage"];
 
+function renderButtons() {
+
+$("#tvshow-buttons").empty();
+
 for (var i = 0; i < topic.length; i++) {
 
       var tvshowbutton = $("<button>");
-      tvshowbutton.addClass("tv show");
+      tvshowbutton.addClass("tvshow");
       tvshowbutton.attr("data-name", topic[i]);
       tvshowbutton.text(topic[i]);
       $("#tvshow-buttons").append(tvshowbutton);
     }
+}
+
+    $("#find-tvshow").on("click", function(event) {
+      event.preventDefault();
+
+      var tvshow = $("#show-input").val().trim();
+      
+      topic.push(tvshow);
+
+      renderButtons();
+
+    });
+
+    renderButtons();
